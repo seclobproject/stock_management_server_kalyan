8592b8dabcf513d89b020857c11809237192faf0
seclobproject <seclobclt@gmail.com>
2024-06-10 10:36:20 +0530

    add production path

--- server.js
+++ server.js
@@ -6,18 +6,18 @@ import path from "path";
 // import dotenv from 'dotenv'
 import { initialize } from "./config/dbConnection.js";
 import { errorHandling } from "./middlewares/error.middleware.js";
-import authRouter from './routes/authRoute.js'
-import categoryRouter from './routes/categoryRoute.js'
-import franchiseRouter from './routes/franchiseRoute.js'
-import productRouter from './routes/productRoute.js'
-import stockRouter from './routes/stockRoute.js'
+import authRouter from "./routes/authRoute.js";
+import categoryRouter from "./routes/categoryRoute.js";
+import franchiseRouter from "./routes/franchiseRoute.js";
+import productRouter from "./routes/productRoute.js";
+import stockRouter from "./routes/stockRoute.js";
 import dashboardRouter from "./routes/dashboardDataRoute.js";
 
 // dotenv.config();
-
+const NODE_ENV = "production";
 const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
 await initialize();
 
@@ -36,10 +36,24 @@ app.use(
   franchiseRouter,
   productRouter,
   stockRouter,
-  dashboardRouter,
+  dashboardRouter
 );
 
 app.use(errorHandling);
+
+if (NODE_ENV == "production") {
+  // app.use(express.static(__dirname + "/frontend/dist"));
+  app.use(express.static("/var/www/seclob/kalyan/admin/frontend/dist"));
+
+  app.get("*", (req, res) => {
+    // res.sendFile(__dirname + "/frontend/dist/index.html");
+    res.sendFile("/var/www/seclob/kalyan/admin/frontend/dist/index.html");
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.status(201).json("Running");
+  });
+}
 
 const port = 8080 || 5000;
 app.listen(port, () => {
