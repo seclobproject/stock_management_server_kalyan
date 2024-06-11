4bfad5fc999b9e394cb46e2252196d338fd2614d
seclobproject <seclobclt@gmail.com>
2024-06-11 18:46:48 +0530

    update dashboard api

--- routes/dashboardDataRoute.js
+++ routes/dashboardDataRoute.js
@@ -3,7 +3,7 @@ import { getDashboardData } from "../controllers/dashboardData.controller.js";
 const router = express.Router();
 
 //........dashboard datas...........//
-router.get("/dashboard", getDashboardData);
+router.get("/api/dashboard", getDashboardData);
 
 
 export default router;
